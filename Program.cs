189ffa8f7adf54ae61e04b3d3875e91bf5b0cf5e
Ankur Seth <iankurseth@gmail.com>
2024-07-20 00:43:07 +0530

    Now parsing args

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+
+void ParseWaxFile (string waxFilePath) {
+	Console.WriteLine($"Filepath: {waxFilePath}");
+}
+
+if (args.Length > 0) {
+	ParseWaxFile(args[0]);
+} else {
+	Console.WriteLine("Usage:\n\twax hello.wax\n");
+}
